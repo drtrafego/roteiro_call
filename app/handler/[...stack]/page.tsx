85ff0c98bf9b4aa9afa29f845a948d9a499169cb
drtrafego <drtrafego@gmail.com>
2026-03-17 10:28:55 -0300

    fix: use any type for handler props for Next.js 15 PageProps compatibility

--- app/handler/[...stack]/page.tsx
+++ app/handler/[...stack]/page.tsx
@@ -1,6 +1,7 @@
 import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "@/lib/stack";
 
-export default function Handler(props: { params: { stack: string[] }; searchParams: Record<string, string> }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Handler(props: any) {
   return <StackHandler app={stackServerApp} fullPage {...props} />;
 }
